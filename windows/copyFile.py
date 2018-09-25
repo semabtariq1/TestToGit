57918a81becbadc0c28cfab0fa6001d5fd5039e2
Semab Tariq <semab.tariq@2ndquadrant.com>
2018-09-25 18:48:14 +0500

    Deleting old python files from windows

--- windows/copyFile.py
+++
@@ -1,7 +0,0 @@
-from distutils.dir_util import copy_tree
-
-
-class CopyFile:
-    def copy(self, src, dest):
-        copy_tree(src, dest)
-        return True
--- windows/currentDateTime.py
+++
@@ -1,5 +0,0 @@
-import time
-
-# Initializing date time
-savedDateTime = time.strftime("%Y%m%d%H%M%S")
-# Process ends
