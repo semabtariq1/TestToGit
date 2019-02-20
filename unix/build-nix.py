d614e98774b2fb739d48c572553345906ab99bf2
Semab Tariq <semab.tariq@2ndquadrant.com>
2019-02-20 10:26:15 +0000

    Unix - Adding pre build checks for postgresVersions.json file

--- unix/build-nix.py
+++ unix/build-nix.py
@@ -1,1 +1,24 @@
-""" Read version file(postgresVersions.json) """
+import json
+import os
+
+
+""" Pre build checks """
+
+
+""" Check file exists, Check if not empty, Read version file(postgresVersions.json) """
+
+try:
+	if os.path.isfile('postgresVersions.json') != True:
+		print('postgresVersions.json file not found ...')
+		exit()
+
+	if os.path.getsize('postgresVersions.json') == 0:
+		print('postgresVersions.json should not be an empty file ...')
+		exit()
+
+	with open('postgresVersions.json', 'r') as componentsInfo:
+		componentsInfo = json.load(componentsInfo)
+
+	print('all ok ...')
+except:
+	print('Please use proper formatting for postgresVersions.json file ...')
