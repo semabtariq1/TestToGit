6ca8b49e1178e9af5fe49aa3855e56d0d66a3a17
Semab Tariq <semab.tariq@2ndquadrant.com>
2019-02-24 12:58:02 +0000

    Unix - Renaming the file postgresVersions.json

--- unix/build-nix.py
+++ unix/build-nix.py
@@ -1,7 +1,7 @@
 import json
 import os
 from config import *
-from local_env import *
+from local_env_pvt import *
 
 
 """ Call Linux build machine to generate builds """
@@ -11,22 +11,22 @@ if platform.system() == 'Darwin' and callLinuxBuildMachine == 1:
 
 """ Pre build checks """
 
-""" Check file exists, Check if not empty, Read version file(postgresVersions.json) """
+""" Check file exists, Check if not empty, Read version file(postgres_versions.json) """
 
 try:
-	if os.path.isfile('postgresVersions.json') != True:
-		print('postgresVersions.json file not found ...')
+	if os.path.isfile('postgres_versions.json') != True:
+		print('postgres_versions.json file not found ...')
 		exit()
 
-	if os.path.getsize('postgresVersions.json') == 0:
-		print('postgresVersions.json should not be an empty file ...')
+	if os.path.getsize('postgres_versions.json') == 0:
+		print('postgres_versions.json should not be an empty file ...')
 		exit()
 
-	with open('postgresVersions.json', 'r') as componentsInfo:
+	with open('postgres_versions.json', 'r') as componentsInfo:
 		componentsInfo = json.load(componentsInfo)
 
 except:
-	print('Please use proper formatting for postgresVersions.json file ...')
+	print('Please use proper formatting for postgres_versions.json file ...')
 
 """ Check share lib folder available or not on give path """
 
@@ -45,9 +45,9 @@ print('Pre build checks are executed successfully ...')
 
 print('Preparing to generate builds now ...')
 
-""" Reading postgreSQ versions from postgresVersions.json """
+""" Reading postgreSQ versions from postgres_versions.json """
 
-with open('postgresVersions.json', 'r') as postgresVersion:
+with open('postgres_versions.json', 'r') as postgresVersion:
 	postgresVersion = json.load(postgresVersion)
 
 for version in postgresVersion:
