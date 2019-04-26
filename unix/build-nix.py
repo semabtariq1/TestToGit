eac96fd5f38d078f50742c71f8218594d6a571f1
Semab Tariq <semab.tariq@2ndquadrant.com>
2019-04-26 11:56:47 +0000

    Unix - Removing inline space gap

--- unix/build-nix.py
+++ unix/build-nix.py
@@ -300,4 +300,4 @@ for postgresVersion in postgresVersions:
                 	        os.system('cd '+ buildDir +'/lib/postgresql && chrpath -r "\${ORIGIN}/../lib/" ./'+ file +" >> "+ logsDir +"/postgreSQL-postgresql-rpaths.log 2>&1")
 
 		# Uncommit it if we have build a new version of TCL 
-                                # os.system('cd /opt/2ndQuadrant/pl-languages/Tcl-8.6/bin && chrpath -r "\${ORIGIN}/../lib/" ./tclsh8.6' " >> "+ dir_logs +"/postgreSQL_postgress_rpaths.log 2>&1")
+                # os.system('cd /opt/2ndQuadrant/pl-languages/Tcl-8.6/bin && chrpath -r "\${ORIGIN}/../lib/" ./tclsh8.6' " >> "+ dir_logs +"/postgreSQL_postgress_rpaths.log 2>&1")
