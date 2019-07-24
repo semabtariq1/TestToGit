302a4df401f2c74433546d28f8e3939d78f4bb75
Semab Tariq <semab.tariq@2ndquadrant.com>
2019-07-24 11:00:21 +0000

    #10061 - Removing build paths information

--- unix/local_env.py
+++ unix/local_env.py
@@ -9,7 +9,6 @@ pl_languages = "/Applications/2ndQuadrant/PostgreSQL/pl-languages"
 python_home = "/Users/2ndquadrant/2UDA/Python-3.4.4/inst"
 openssl_home = "/Users/2ndquadrant/2UDA/openssl-1.0.2g/inst"
 shareLib = "/Users/2ndquadrant/pythonAutomation/srcBuild"
-mac_build_path = "/Users/2ndquadrant/semabHome/main"
 signingPasswordRoot = '/Users/2ndquadrant/semabHome/automation/codesign/signing'
 bitrockInstallation = '/Applications/BitRockInstallBuilderProfessional16'
 omnidbUrl = 'https://omnidb.org/dist/2.16.0/omnidb-app_2.16.0-mac.dmg'
@@ -21,23 +20,12 @@ if os_name == "Linux":
     python_home = "/opt/Python-3.4.4/inst"
     openssl_home = "/opt/openssl-1.0.2g/inst"
     shareLib = "/opt/PGInstaller/Python-automation-code/linux_share_lib1"
-    mac_address = "2ndquadrant@208.52.185.118"
     signingPasswordRoot = '/opt/semabHome/automation/postgres-installer-private/signing'
     bitrockInstallation = '/opt/semabHome/bitrock/installation/'
 
 else:
     """ Variables that are used to call linux machine and execute build code are ipAddr, userName, buildCode """
 
-    ipAddr = "52.221.191.183"
-    userName = "build"
-    buildCode = "/opt/semabHome/postgresBuilds/dev/PGInstaller/unix"
-    installer_build = "/Users/2ndquadrant/semabHome/temp-installer-code/Builds"
-    mac_oldbk = "/Users/2ndquadrant/semabHome/oldbk/thurs12sept2018"
-    installer_code = "/Users/2ndquadrant/semabHome/temp-installer-code"
-
-password = ""
-with open(shareLib +"/emailPassword.txt") as file:
-    password = file.readline().replace('\n', "")
 
 """ Some general variables should not be modified in any case """
 root = os.path.dirname(os.path.abspath(__file__))
