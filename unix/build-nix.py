8319062b6c01a3b0f843289e76bbd8c958281907
Haroon <haroon@2ndQuadrant.com>
2019-07-24 14:32:27 -0400

    Reformatting the PRE-BUILD checks

--- unix/build-nix.py
+++ unix/build-nix.py
@@ -18,22 +18,36 @@ print(' ************************************* \n')
 
 
 
-""" Check file exists, Check if not empty, Read version file(postgres_versions.json) """
+# Checking all the required file, directories exists or not
 
-try:
-	if os.path.isfile('postgres_versions.json') != True:
-		print('postgres_versions.json file not found ...')
+if os.path.isfile('postgres_versions.json') != True:
+	print('postgres_versions.json ... NOT FOUND')
+	exit()
+else:
+	print('postgres_versions.json ... OK')
+
+if os.path.getsize('postgres_versions.json') == 0:
+	print('postgres_versions.json ... CONTENT NOT FOUND')
+	exit()
+else:
+	print('postgres_versions.json ... CONTENT FOUND')
+
+if os.path.exists(shareLib):
+	print('Shared libraries folder ... OK')
+
+	if not os.listdir(shareLib):
+		print('Shared libraries folder ... CONTENT NOT FOUND')
 		exit()
+	else:
+		print('Shared libraries folder ... CONTENT FOUND ')
+else:
+	print('Shared libraries folder ... NOT FOUND')
+	exit()
 
-	if os.path.getsize('postgres_versions.json') == 0:
-		print('postgres_versions.json should not be an empty file ...')
-		exit()
 
-	with open('postgres_versions.json', 'r') as componentsInfo:
-		componentsInfo = json.load(componentsInfo)
+with open('postgres_versions.json', 'r') as componentsInfo:
+	componentsInfo = json.load(componentsInfo)
 
-except:
-	print('Please use proper formatting for postgres_versions.json file ...')
 
 """ Check share lib folder available or not on give path """
 
