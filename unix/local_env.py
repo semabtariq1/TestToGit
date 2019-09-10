694ccae27805e5af4a1caa85c89d1c91a6e38fe4
Semab Tariq <semab.tariq@2ndquadrant.com>
2019-09-10 06:58:19 +0000

    Adding info for 12beta4 build

--- unix/local_env.py
+++ unix/local_env.py
@@ -3,7 +3,7 @@ import os
 
 os_name = platform.system()
 
-projectName = '12beta3'
+projectName = '12beta4'
 
 pl_languages = "/Applications/2ndQuadrant/PostgreSQL/pl-languages"
 python_home = "/Users/2ndquadrant/2UDA/Python-3.4.4/inst"
