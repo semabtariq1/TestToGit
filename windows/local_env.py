3ddf043f05e2860fde0e6c011b8dbae4bdf05287
Semab Tariq <semab.tariq@2ndquadrant.com>
2019-09-10 07:04:00 +0000

    Adding 12beta4 build properties

--- windows/local_env.py
+++ windows/local_env.py
@@ -2,7 +2,7 @@ import platform
 import os
 
 
-projectName = 'test'
+projectName = '12beta4'
 
 vsCommandPromptX64    = "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC"
 shareLib                 = "C:\\build-python-dev\\PGInstaller\\windows\\winShareLib"
