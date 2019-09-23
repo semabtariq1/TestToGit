1b80ba0ba371cc85a52d6378c448eaf608993d67
Semab Tariq <semab.tariq@2ndquadrant.com>
2019-09-23 20:28:05 +0000

    Updating Windows signing password file path

--- windows/local_env.py
+++ windows/local_env.py
@@ -7,7 +7,7 @@ projectName = '12beta4'
 vsCommandPromptX64    = "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC"
 shareLib                 = "C:\\build-python-dev\\PGInstaller\\windows\\winShareLib"
 windowsCmd              = 'C:\\Windows\\system32\\cmd.exe'
-signingPasswordRoot = 'C:\\semabHome\\codesign\\signing'
+signingPasswordRoot = 'C:\\signing'
 bitrockInstallation = 'C:\\installBuilder'
 projectFileName = 'PGInstaller.xml'
 
