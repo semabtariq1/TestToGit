3f9f5325df7456134dbc8234d3c281856f63a70a
Haroon <haroon@2ndQuadrant.com>
2019-11-11 17:49:44 -0500

    Copy serverctl.vbs into installdir/bin

--- unix/build-nix.py
+++ unix/build-nix.py
@@ -529,6 +529,10 @@ for postgresVersion in postgresVersions:
 	print('Copy openssl/lib into buildDir/lib ...')
 	os.system('cp -rv '+ openssl_home +'/lib/* '+ buildDir +'/lib/ >> '+ logsDir +'/copy.log')
 
+	# Copy serverctl.vbs into bin
+	if osType == 'Windows':
+		os.system('cp '+ shareLib +'/scripts/serverctl.vbs '+ buildDir +'/bin')
+
 	if postgresVersion['LLVM']    == '1' and  osType == 'Linux':
             os.system('cp '+ llvmPath +'/lib/libLLVM* '+ buildDir +'/lib && cp '+ llvmPath +'/local/lib64/libstdc++.so.6 '+ buildDir +'/lib')
 
