268eb617c43a598f32f63702d413175b404a8c44
Semab Tariq <semab.tariq@2ndquadrant.com>
2019-11-11 07:07:27 +0000

    Windows - Updating shareLibs path

--- windows/local_env.py
+++ windows/local_env.py
@@ -5,7 +5,7 @@ import os
 projectName = '12.0'
 
 vsCommandPromptX64    = "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC"
-shareLib                 = "C:\\build-python-dev\\PGInstaller\\windows\\winShareLib"
+shareLib                 = "C:\\winShareLib"
 windowsCmd              = 'C:\\Windows\\system32\\cmd.exe'
 signingPasswordRoot = 'C:\\signing'
 bitrockInstallation = 'C:\\installBuilder'
