f4f49ab3f11f1738e015164738331db928532c64
Haroon <haroon@2ndQuadrant.com>
2020-02-11 00:09:24 -0500

    Preparing the config files for 13022020 Stable release of PostgreSQL

--- unix/local_env.py
+++ unix/local_env.py
@@ -3,7 +3,7 @@ import os
 
 os_name = platform.system()
 
-projectName = '14112019'
+projectName = '13022020'
 
 pl_languages = "/Applications/2ndQuadrant/PostgreSQL/pl-languages"
 python_home = "/Users/2ndquadrant/2UDA/Python-3.4.4/inst"
