41ca47850c03799ecc6fd743e5e4eecb57c7a881
Haroon <haroon@2ndQuadrant.com>
2020-03-25 01:49:06 -0400

    Updating the project name for 26/03/2020 Out Of Cycle release by 2ndQuadrant

--- unix/local_env.py
+++ unix/local_env.py
@@ -3,7 +3,7 @@ import os
 
 os_name = platform.system()
 
-projectName = '13022020'
+projectName = '26032020'
 
 pl_languages = "/Applications/2ndQuadrant/PostgreSQL/pl-languages"
 python_home = "/Users/2ndquadrant/2UDA/Python-3.4.4/inst"
